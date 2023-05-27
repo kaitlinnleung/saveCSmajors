670c35ee44c82a8d978ca7002aaf63e385a37218
Kaitlin Leung <leungkn@uci.edu>
2023-05-27 11:43:02 -0700

    Fetched API, added specialization page, added functionality with Javascript functions, resources page completed
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Anna Nguyen anguyenmh9@gmail.com
    Co-authored by: Sansshita Baskaran sansshita12@gmail.com

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const PETERPORTAL_BASE_URL = "https://api.peterportal.org/rest/v0/";
+//dictionary for class info
+var info = {};
+//dictionary for class count
+var count = {};
+var valid = [ "I&C SCI", "COMPSCI", "IN4MATX" ]
+
+
 function dropdown() {
     var x = document.getElementById("myLinks");
 
@@ -8,12 +16,32 @@ function dropdown() {
     }
 }
 
-function quizpage() {
+function AddToCart() {
+    var shoppingCart = document.getElementsByName("courses").value;
     
+    for (var i = 0; i < shoppingCart.length; i++) {
+        if (shoppingCart[i].checked) {
+            if (shoppingCart[i] in count) {
+                shoppingCart[i]++;
+            } else {
+                shoppingCart[i] = 1;
+            }
+        }
+    }
 }
 
-function resourcespage() {
-}
+function loadquiz() {
+    fetch("https://api.peterportal.org/rest/v0/courses/all").then(res=>res.json()).then(response =>{
+        console.log(response)
 
-function aboutpage() {
+        if ( response.department in validclasses )
+        {
+            info[ response[id] ] = [ response[departmemnt], response[number], response[title], response[description] ];
+        }
+    })
+    var objkey = Object.keys( info );
+    for(var i = 0; i < Object.keys( info ).length; i++) {
+        var obj = info[ objkey[ i ] ];
+        document.body.innerHTML += '<form><input type="checkbox" name="courses" id="obj[id]" value="cs121" onclick="return AddToCart();"><label for="course1">CS 121</label></form>'//call to dictionary
+     }
 }
