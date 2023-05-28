ae5ff8421a271236a4bfbf660f2b1a226549625d
Kaitlin Leung <leungkn@uci.edu>
2023-05-27 19:53:41 -0700

    trying to update checkbox counter
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Anna Nguyen anguyenmh9@gmail.com

--- script.js
+++ script.js
@@ -198,13 +198,3 @@ function loadquiz() {
     })
 
 }
-
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
-
-function myFunction2() {
-    var popup = document.getElementById("myPopup2");
-    popup.classList.toggle("show");
-  }
