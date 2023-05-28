b53464558f160430b6a1ca288eb5392dff2ee88b
Kaitlin Leung <leungkn@uci.edu>
2023-05-27 22:33:50 -0700

    working checkout and increment algorithm, changed some ui features, and functional results page
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Anna Nguyen anguyenmh9@gmail.com

--- script.js
+++ script.js
@@ -6,35 +6,49 @@ var specializations = {
     "COMPSCI111": ["Visual Computing"], 
     "COMPSCI112": ["Visual Computing"], 
     "COMPSCI114": ["Visual Computing"], 
-    "COMPSCI116": ["Visual Computing", "Intellient Systems"],   
+    "COMPSCI116": ["Visual Computing", "Intelligent Systems"],   
     "COMPSCI117": ["Visual Computing"], 
-    "COMPSCI121": ["Information", "Intelligent Systems"],  
-    "COMPSCI122": ["Information"],  
-    "COMPSCI122B": ["Information"], 
-    "COMPSCI125": ["Information", "Intelligent Systems"],
+    "COMPSCI118": ["Visual Computing"], 
+    "COMPSCI121": ["Req Information", "Intelligent Systems"],  
+    "COMPSCI122A": ["Req Information"],  
+    "COMPSCI122B": ["One Information", "Information"], 
+    "COMPSCI122C": ["One Information", "Information"], 
+    "COMPSCI122D": ["One Information", "Information"], 
+    "COMPSCI125": ["One Information", "Information", "Intelligent Systems"],
     "COMPSCI131": ["Systems and Software"],
     "COMPSCI132": ["Information", "Networked Systems"],
     "COMPSCI133": ["Networked Systems"],
     "COMPSCI134": ["Information", "Networked Systems"],
     "COMPSCI141": ["Information", "Systems and Software"],
     "COMPSCI142A": ["Information", "Systems and Software"],
+    "COMPSCI142B": ["Systems and Software"],
     "COMPSCI143A": ["Information", "Networked Systems", "Systems and Software"],
     "COMPSCI143B": ["Systems and Software"],
     "COMPSCI143A": ["Architecture & Embedded Systems"],
     "COMPSCI143B": ["Architecture & Embedded Systems"],
+    "COMPSCI145": ["Architecture & Embedded Systems"],
+    "COMPSCI147": ["Architecture & Embedded Systems"],
     "COMPSCI151": ["Architecture & Embedded Systems"],
     "COMPSCI152": ["Architecture & Embedded Systems"],
+    "COMPSCI153": ["Architecture & Embedded Systems"],
     "COMPSCI154": ["Architecture & Embedded Systems"],
     "COMPSCI162": ["Algorithms", "Intelligent Systems"],
     "COMPSCI163": ["Algorithms", "Information", "Intelligent Systems"],
+    "COMPSCI164": ["Algorithms", "Intelligent Systems"],
+    "COMPSCI165": ["Algorithms", "Information"],
+    "COMPSCI166": ["Algorithms"],
     "COMPSCI167": ["Algorithms", "Information"],
     "COMPSCI169": ["Algorithms", "Intelligent Systems"],
-    "COMPSCI171": ["Intelligent Systems"],
-    "COMPSCI175": ["Intelligent Systems"],
+    "COMPSCI171": ["Req Intelligent Systems"],
+    "COMPSCI172B": ["Bioinformatcs"],
+    "COMPSCI172C": ["Bioinformatcs"],
+    "COMPSCI175": ["Req Intelligent Systems"],
     "COMPSCI177": ["Intelligent Systems"],
-    "COMPSCI178": ["Information", "Intelligent Systems"],
-    "COMPSCI184A": ["Bioinformatics"],
+    "COMPSCI178": ["Req Information", "Req Intelligent Systems"],
+    "COMPSCI179": ["One Information", "Information", "Intelligent Systems"], 
+    "COMPSCI184A": ["Req Bioinformatics"],
     "COMPSCI184C": ["Bioinformatcs"],
+    "I&CSCI45J": ["Visual Computing"],
     "I&CSCI162": ["Visual Computing"] 
 };
 //dictionary for class count
@@ -42,14 +56,19 @@ var count = {
     "Algorithms": 0,
     "Architecture & Embedded Systems": 0,
     "Bioinformatics": 0,
+    "Req Bioinformatics": 0,
     "Information": 0,
+    "Req Information": 0,
+    "One Information": 0,
     "Intelligent Systems": 0,
+    "Req Intelligent Systems": 0,
     "Networked Systems": 0,
     "Systems and Software": 0,
     "Visual Computing": 0,
+    "General CS": 0
 };
 
-
+var spec_list = [];
 
 
 function dropdown() {
@@ -94,28 +113,25 @@ function collapsible() {
 */
 
 function popupFunc() {
-    console.log("hi");
     var popup = document.getElementById(pid);
     popup.classList.toggle("show");
 }
 
-function crement( cid ) {
-    console.log( cid );
-    var specs = specializations[cid]
-    var checker = document.getElementsByTagName("input");
-    for (spec in specs){
-        if ( checker.checked = true )
-        {
-            count[ specs[spec] ]++;
-        
-        }
-        else
-        {
-            count[ specs[spec] ]--;
-        }
-        console.log(specializations[cid][spec]);
-        console.log(count[specs[spec]]);
+function increment( cid ) {
+    if (cid in specializations) {
+        var specs = specializations[cid];
+        if (document.getElementById(cid).checked) {
+            console.log(cid)
+            // if specs is not found then gen cs
+            for (spec in specs){
+                count[ specs[spec] ]++;
+            }
+        } 
     }
+    else {
+        count[ "General CS" ]++;
+    }
+}
     
     /*
     for ( var i = 0; i < spec.length; ++i )
@@ -131,33 +147,107 @@ function crement( cid ) {
         }
         console.log( count[ spec[i] ] );
     }*/
+
+
+function checkout() {
+    //console.log("checkout working");
+    for ( val of Object.values( info ) )
+    {
+        increment( val[0] );
+    }
+    console.log(count);
+    if ( count["Algorithms"] >= 4 ) {
+        spec_list.push( "Algorithms" );
+
+    }
+    
+    if ( count["Architecture & Embedded Systems"] >= 4 ) {
+        spec_list.push( "Architecture & Embedded Systems" );
+    }
+
+    if ( count["Bioinformatics"] >= 3 && count["Req Bioinformatics"] >= 1 ) {
+        spec_list.push( "Bioinformatics" );
+    }
+
+    if ( count["Information"] >= 4 && count["One Information"] >= 1 && count["Req Information"] >= 3 ) {
+        spec_list.push( "Information" );
+    }
+
+    if ( count["Intelligent Systems"] >= 3 && count["Req Intelligent Systems"] >= 3) {
+        spec_list.push( "Intelligent Systems" );
+    }
+
+    if ( count["Networked Systems"] >= 4) {
+        spec_list.push( "Networked Systems" );
+    }
+
+    if ( count["Systems and Software"] >= 3) {
+        spec_list.push( "Systems and Software" );
+    }
+
+    if ( count["Visual Computing"] >= 4) {
+        spec_list.push( "Visual Computing" );
+    }
+    if ( spec_list.length == 0 ) {
+        spec_list.push( "General CS" );
+    }   
+
+    document.body.innerHTML = `
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <div class="navbar">
+            <img class="active" id="dbhimage" src="UCI17_Bren_ICS_2L_white-1200.png" alt ="UCI Donald Bren School of Information & Computer Sciences"/>
+            <div id="myLinks" class="links">
+                <a href="index.html">Home</a>
+                <a href="quiz.html">Quiz</a>
+                <a href="specializations.html">Specializations</a>
+                <a href="resources.html">Resources</a>
+                <a href="aboutus.html">About Us</a>
+            </div>
+            <a href="javascript:void(0);" class="icon" onclick="dropdown()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+
+        <h1>Specialization Results: </h1>
+
+    `
+    for ( var i = 0; i < spec_list.length; i++ )
+    {
+        document.body.innerHTML += `<h2>` + spec_list[i] + `</h2>`
+    }
 }
 
 function loadquiz() {
     fetch("https://api.peterportal.org/rest/v0/courses/all").then(res=>res.json()).then(response =>{
-        for ( var i = 1173; i <= 1329; i++ ) //cs
+        //1225
+        // cs 161 = 1206
+        // ics 45j = 3240
+        for ( var i = 1173; i <= 1225; i++ ) //cs
         {
-            if (response[i].id in specializations) {
+            if (i != 1206) {
                 info[ String(response[i].id) ] = [ response[i].id, response[i].department, response[i].number, response[i].title, response[i].description ];
             }
         }
         info[ String(response[3220].id) ] = [ response[3220].id, response[3220].department, response[3220].number, response[3220].title, response[3220].description ];
-        var objkey = Object.keys( info );
+        info[ String(response[3240].id) ] = [ response[3240].id, response[3240].department, response[3240].number, response[3240].title, response[3220].description ];
+        //var objkey = Object.keys( info );
         const formelement = document.createElement( "form" );
         var pidcount = 1
+
         for( [id, department, number, title, description] of Object.values(info) ) {
             const div = document.createElement( "div" );
             const checkbox = document.createElement( "input" );
             checkbox.type = "checkbox";
-            checkbox.setAttribute("id", id);
+            checkbox.setAttribute("id", id);     
             
-
+            
             checkbox.onclick = () => {
                 //console.log(id);
                 //console.log(specializations[id]);
-                checkbox.classList.toggle( crement( id ) );
+                checkbox.classList.toggle( console.log( "checkbox click" ) );
                 //count[ class_specializations ]++;
             }
+            
             div.appendChild( checkbox );
 
             const label = document.createElement( "label" );
@@ -187,12 +277,15 @@ function loadquiz() {
             formelement.append( div );
             pidcount++;
         }
+
         document.body.appendChild( formelement );
 
         const buttonsect = document.createElement( "section" );
         const submitbutton = document.createElement( "button" );
         submitbutton.innerText = "Checkout"
-        submitbutton.onclick = "checkout();"
+        submitbutton.onclick = () => {
+            checkout();
+        }
         buttonsect.appendChild( submitbutton )
         document.body.appendChild( buttonsect );
     })
